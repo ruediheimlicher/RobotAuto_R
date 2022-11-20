d58f6138b90a69fdac3e9421f21353cf55783d10
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2022-11-20 10:57:24 +0100

    writeservoValues neu setupPinModes neu valueInt auf 180 red

--- RobotAuto_R.ino
+++ RobotAuto_R.ino
@@ -31,7 +31,7 @@ void playTon(int ton);
 #define START_TON 0
 #define LICHT_ON 1
 
-uint8_t expolevel = 3;
+uint8_t expolevel = 0;
 
 uint16_t ubatt = 0;
 
@@ -70,6 +70,31 @@ unsigned long previousTimeInMilli = millis();
 
 
 
+void writeServoValues(int servoIndex, int value)
+{
+  if (recordSteps)
+  {
+    RecordedStep recordedStep;       
+    if (recordedSteps.size() == 0) // We will first record initial position of all servos. 
+    {
+      for (int i = 0; i < servoPins.size(); i++)
+      {
+        recordedStep.servoIndex = i; 
+        recordedStep.value = servoPins[i].servo.read(); 
+        recordedStep.delayInStep = 0;
+        recordedSteps.push_back(recordedStep);         
+      }      
+    }
+    unsigned long currentTime = millis();
+    recordedStep.servoIndex = servoIndex; 
+    recordedStep.value = value; 
+    recordedStep.delayInStep = currentTime - previousTimeInMilli;
+    recordedSteps.push_back(recordedStep);  
+    previousTimeInMilli = currentTime;         
+  }
+  Serial.printf("pin: [%d] servoindex: [%d] value: [%d]  \n",servoPins[servoIndex].servoPin , servoIndex, value);
+  servoPins[servoIndex].servo.write(value);   
+}
 
 
 
@@ -77,7 +102,12 @@ unsigned long previousTimeInMilli = millis();
 //Must match the sender structure
 typedef struct canal_struct 
 {
-  uint16_t canalarray[4] ;
+  uint16_t lx;
+  uint16_t ly;
+  uint16_t rx;
+  uint16_t ry;
+ 
+
   int x;
   int y;
 } canal_struct;
@@ -95,14 +125,49 @@ void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   Serial.print(" ");
   */
   
-   Serial.print(map(canaldata.x,0,4095, 0,255));
+   Serial.print(canaldata.lx);
   Serial.print(" ");
-  Serial.println(map(canaldata.y,0,4095, 0,255));
+  Serial.print(map(canaldata.lx,0,4095, 0,255));
   //Serial.println();
+  //Serial.print(" ");
+  //Serial.print(map(canaldata.ly,0,4095, 0,255));
+  
+  int valueInt = map(canaldata.lx,0,4095, 0,180);
+  Serial.print(" valueInt: ");
+  Serial.print(valueInt);
 
+  
+digitalWrite(13,!digitalRead(13));
+  int expovalue = 0;
+  if (valueInt > maxwinkel/2)
+  {
+     expovalue = maxwinkel/2 +  expoarray[expolevel][valueInt - maxwinkel/2];
+  }
+  else
+  {
+     expovalue = maxwinkel/2 -  expoarray[expolevel][maxwinkel/2 - valueInt ];
+  }
+  
+   Serial.print(" expovalue: ");
+  Serial.println(expovalue);
 
+  writeServoValues(1,90 + ((180 - expovalue) - 90)/1); // red Ausschlaege
+    
 
 }
+
+void setUpPinModes()
+{
+  for (int i = 0; i < servoPins.size(); i++)
+  {
+    servoPins[i].servo.attach(servoPins[i].servoPin);
+    servoPins[i].servo.write(servoPins[i].initialPosition);    
+  }
+ 
+  
+}
+
+
  
 void setup() {
   //Initialize Serial Monitor
@@ -116,7 +181,11 @@ void setup() {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  
+
+  setUpPinModes();
+  pinMode(12, OUTPUT);
+  pinMode(14, OUTPUT);
+   pinMode(13, OUTPUT);
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
